2f5612572dfa484de09b4a842e49bd5f77d32cad
0rGaan1c <nathan4drake1999@gmail.com>
2021-09-25 16:57:28 +0530

    added error message

--- src/QuizCategory.js
+++ src/QuizCategory.js
@@ -1,16 +1,27 @@
-import React, { useRef } from "react";
+import React, { useState, useRef } from "react";
 import "./QuizCategory.css";
 
 const QuizCategory = ({ data }) => {
   const numberRef = useRef(null);
+  const [isValidRange, setIsValidRange] = useState(true);
+  const [isValidInput, setIsValidInput] = useState(true);
 
   const handleClick = () => {
     const numberOfQuestions = numberRef.current.value;
     if (!numberOfQuestions) {
       console.log("please enter a valid number");
+      setIsValidInput(false);
+      setTimeout(() => {
+        setIsValidInput(true);
+      }, 2000);
+      return;
     }
-    if (numberOfQuestions < 5 && numberOfQuestions > 25) {
+    if (numberOfQuestions < 5 || numberOfQuestions > 25) {
       console.log("choose between 5 and 25");
+      setIsValidRange(false);
+      setTimeout(() => {
+        setIsValidRange(true);
+      }, 2000);
       return;
     }
   };
@@ -19,6 +30,12 @@ const QuizCategory = ({ data }) => {
     <>
       <div className="quiz-category">
         <img src={data.img} alt={data.type} />
+        <p className={isValidInput ? "error-hide" : "error-show"}>
+          Please enter a valid number
+        </p>
+        <p className={isValidRange ? "error-hide" : "error-show"}>
+          Choose between 5 and 25 questions.
+        </p>
         <div className="quiz-start">
           <div className="quiz-mode">
             <select name="difficulty" id="difficulty">
