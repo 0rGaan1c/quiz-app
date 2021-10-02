7944d3a9c346f9320101f79637dc6ecadb680ae8
0rGaan1c <nathan4drake1999@gmail.com>
2021-10-02 11:01:21 +0530

    fixed a minor bug

--- src/components/QuizQuestions.js
+++ src/components/QuizQuestions.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { useEffect } from "react/cjs/react.development";
+import React, { useState, useEffect } from "react";
 import "./QuizQuestions.css";
 
 // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
